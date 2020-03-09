98ba4a5a31caa8d9a539cc6b523ecce4330331d2
kukdu <kukduapp@gmail.com>
2020-03-09 12:49:18 +0530

    Task 1 : login/editprofile/logout

--- admin/application/config/routes.php
+++ admin/application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+
+
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+//$route['translate_uri_dashes'] = FALSE;
+
+$route['loginMe'] = 'login/loginMe';
+$route['editProfile'] = 'users/index';
+
+$route['itemListing'] = 'items/index';
